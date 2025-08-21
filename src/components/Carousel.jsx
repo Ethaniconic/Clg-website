457ff4fc2f574ff47e27398dc006ab963f7a047c
Ethaniconic <e10.anthony345@gmail.com>
2025-08-21 11:56:10 +0530

    Added lazy loading for images

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -57,7 +57,7 @@ const Carousel = () => {
                             src={slide.img}
                             className="object-fit w-full h-full mx-auto"
                             alt={`Slide ${idx + 1}`}
-                            loading="eager"
+                            loading="lazy"
                         />
                         <div className="absolute left-4 right-4 bottom-4 flex justify-between">
                             <button
