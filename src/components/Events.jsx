e6d98da0e194279e822de7e7873b36667a2ffcce
Nikita Bhushanwar <nikitabhushanwar03@gmail.com>
2025-08-25 12:36:10 +0530

    Added images in events page

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -2,22 +2,22 @@ import React from "react";
 
 const images = [
   {
-    src: "https://images.unsplash.com/photo-1506744038136-46273834b3fb?auto=format&fit=crop&w=600&q=80",
+    src: "src/assets/IDEA3_1.webp",
     caption: "Annual Tech Fest 2024",
     ratio: "row-span-2 col-span-2",
   },
   {
-    src: "https://images.unsplash.com/photo-1465101046530-73398c7f28ca?auto=format&fit=crop&w=400&q=80",
+    src: "src/assets/IDEA3_2.webp",
     caption: "Research Symposium",
     ratio: "row-span-1 col-span-1",
   },
   {
-    src: "https://images.unsplash.com/photo-1519125323398-675f0ddb6308?auto=format&fit=crop&w=400&q=80",
+    src: "src/assets/IDEA3_3.webp0",
     caption: "Alumni Meet",
     ratio: "row-span-1 col-span-1",
   },
   {
-    src: "https://images.unsplash.com/photo-1517649763962-0c623066013b?auto=format&fit=crop&w=400&q=80",
+    src: "src/assets/IDEA3_4.webp",
     caption: "Sports Day",
     ratio: "row-span-2 col-span-1",
   },
